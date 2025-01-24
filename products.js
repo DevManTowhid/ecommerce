cf1d61d723eca13412f772121d7f0b1eb900f0e1
DevManTowhid <mdtowhidulalam364@gmail.com>
2025-01-25 01:38:58 +0600

    frontend added

---
+++ products.js
@@ -0,0 +1,305 @@
+const products = [
+    {
+        id: 1,
+        name: 'Laptop',
+        price: 1000,
+        stock: 10
+
+    },
+    {
+        id: 2,
+        name: 'Desktop',
+        price: 1500,
+    },
+    {
+        id: 3,
+        name: 'Phone',
+        price: 500,
+        stock: 20
+        
+    },
+    {
+        id: 4,
+        name: 'Tablet',
+        price: 600,
+        stock: 30
+    }
+    ,
+    {
+        id: 5,
+        name: 'Watch',
+        price: 300,
+        stock: 40
+    },
+    {
+        id: 6,
+        name: 'Camera',
+        price: 700,
+        stock: 50
+    },
+    {
+        id: 7,
+        name: 'Headphone',
+        price: 200,
+        stock: 60
+    },
+    {
+        id: 8,
+        name: 'Speaker',
+        price: 400,
+        stock: 70
+    },
+    {
+        id: 9,
+        name: 'Keyboard',
+        price: 50,
+        stock: 80
+    },
+    {
+        id: 10,
+        name: 'Mouse',
+        price: 20,
+        stock: 90
+    },
+    {
+        id: 11,
+        name: 'Monitor',
+        price: 300,
+        stock: 100
+    },
+    {
+        id: 12,
+        name: 'Printer',
+        price: 150,
+        stock: 110
+    },
+    {
+        id: 13,
+        name: 'Scanner',
+        price: 100,
+        stock: 120
+    },
+    {
+        id: 14,
+        name: 'USB',
+        price: 10,
+        stock: 130
+    },
+    {
+        id: 15,
+        name: 'HDMI',
+        price: 5,
+        stock: 140
+    },
+    {
+        id: 16,
+        name: 'VGA',
+        price: 5,
+        stock: 150
+    },
+    {
+        id: 17,
+        name: 'DVI',
+        price: 5,
+        stock: 160
+    },
+    {
+        id: 18,
+        name: 'Ethernet',
+        price: 5,
+        stock: 170
+    },
+    {
+        id: 19,
+        name: 'Router',
+        price: 50,
+        stock: 180
+    },
+    {
+        id: 20,
+        name: 'Switch',
+        price: 100,
+        stock: 190
+    },
+    {
+        id: 21,
+        name: 'Hub',
+        price: 10,
+        stock: 200
+    },
+    {
+        id: 22,
+        name: 'Modem',
+        price: 20,
+        stock: 210
+    },
+    {
+        id: 23,
+        name: 'Access Point',
+        price: 30,
+        stock: 220
+    },
+    {
+        id: 24,
+        name: 'Firewall',
+        price: 50,
+        stock: 230
+    },
+    {
+        id: 25,
+        name: 'Server',
+        price: 500,
+        stock: 240
+    },
+    {
+        id: 26,
+        name: 'Storage',
+        price: 1000,
+        stock: 250
+    },
+    {
+        id: 27,
+        name: 'UPS',
+        price: 200,
+        stock: 260
+    },
+    {
+        id: 28,
+        name: 'Generator',
+        price: 5000,
+        stock: 270
+    },
+    {
+        id: 29,
+        name: 'Cable',
+        price: 5,
+        stock: 280
+    },
+    {
+        id: 30,
+        name: 'Adapter',
+        price: 10,
+        stock: 290
+    },
+    {
+        id: 31,
+        name: 'Battery',
+        price: 50,
+        stock: 300
+    },
+    {
+        id: 32,
+        name: 'Charger',
+        price: 20,
+        stock: 310
+    },
+    {
+        id: 33,
+        name: 'Case',
+        price: 30,
+        stock: 320
+    },
+    {
+        id: 34,
+        name: 'Cooler',
+        price: 40,
+        stock: 330
+    },
+    {
+        id: 35,
+        name: 'Fan',
+        price: 10,
+        stock: 340
+    },
+    {
+        id: 36,
+        name: 'Heatsink',
+        price: 20,
+        stock: 350
+    },
+    {
+        id: 37,
+        name: 'Thermal Paste',
+        price: 5,
+        stock: 360
+    },
+    {
+        id: 38,
+        name: 'RAM',
+        price: 50,
+        stock: 370
+    },
+    {
+        id: 39,
+        name: 'SSD',
+        price: 100,
+        stock: 380
+    },
+    {
+        id: 40,
+        name: 'HDD',
+        price: 50,
+        stock: 390
+    },
+    {
+        id: 41,
+        name: 'Motherboard',
+        price: 100,
+        stock: 400
+    },
+    {
+        id: 42,
+        name: 'CPU',
+        price: 200,
+        stock: 410
+    },
+    {
+        id: 43,
+        name: 'GPU',
+        price: 300,
+        stock: 420
+    },
+    {
+        id: 44,
+        name: 'PSU',
+        price: 100,
+        stock: 430
+    },
+    {
+        id: 45,
+        name: 'Monitor Stand',
+        price: 20,
+        stock: 440
+    },
+    {
+        id: 46,
+        name: 'Desk',
+        price: 50,
+        stock: 450
+    },
+    {
+        id: 47,
+        name: 'Chair',
+        price: 30,
+        stock: 460
+    },
+    {
+        id: 48,
+        name: 'Lamp',
+        price: 10,
+        stock: 470
+    },
+    {
+        id: 49,
+        name: 'Clock',
+        price: 5,
+        stock: 480
+    },
+    {
+        id: 50,
+        name: 'Calendar',
+        price: 5,
+        stock: 490
+    }
+
+]
